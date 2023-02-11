458a85cfa43235c1f53691279599a30e92a7c6ec
Arshad Masood <amasoood@xellagain.com>
2023-02-11 14:20:26 -0800

    Updated code to read secret from KV and limit what files can be downloaded

--- github-util/fn-githubproxy/fn-githubproxy.cs
+++ github-util/fn-githubproxy/fn-githubproxy.cs
@@ -1,67 +1,72 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Net;
-using Microsoft.Extensions.Primitives;
+using System;
+using System.IO;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace patriot.mxdr.githubutil
 {
-    public static class githubproxy
+    public class githubproxy
     {
+        private UrlChecker checker;
+        private string githubAccessToken;
+
+        public githubproxy(IConfiguration config)
+        {
+            checker = new UrlChecker(config);
+            githubAccessToken = config.GetSection("githubAccessToken").Value;
+        }
+
+        private async Task<string> DownloadFileFromGitHub(string gitHubURL)
+        {
+            string strAuthHeader = "token " + githubAccessToken;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3.raw");
+            client.DefaultRequestHeaders.Add("Authorization", strAuthHeader);
+            Stream stream = await client.GetStreamAsync(gitHubURL);
+            StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
         [FunctionName("fn-githubproxy")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger received.");
-
-            string githubAccessToken = "github_patto_11A32DTVY0C1dLUZFNSWdh_kh7wZ5zClgoKLjE1hCM6A0sYKXuYRMib8mB5eu7hPXw432PU76XK3aytOI1";
+    
+            log.LogInformation("C# HTTP trigger received");
 
             string code = null;
             string gitHubURL = null;
-            Exception error = null;
+            string errorMessage = null;
+            ObjectResult result = null;
 
             try
             {
                 gitHubURL = req.Query["gitHubURL"];
-                string strAuthHeader = "token " + githubAccessToken;
-
-                HttpClient client = new HttpClient();   
-                client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3.raw");
-                client.DefaultRequestHeaders.Add("Authorization", strAuthHeader);
-
-                Stream stream = await client.GetStreamAsync(gitHubURL);
-                StreamReader reader = new StreamReader(stream);
-                code = reader.ReadToEnd();
+                bool isUrlAllowed = checker.CheckUrl(gitHubURL);
+                if (isUrlAllowed)
+                    code = await DownloadFileFromGitHub(gitHubURL);
+                else
+                    errorMessage = "Url is not allowed";
             }
             catch (Exception e)
             {
-                error = e;
-                // empty code var will signal error
+                errorMessage = e.Message;
             }
-            ObjectResult result = null;
-            if (null != code)
-            {
-                result = new OkObjectResult(code);
-            }
-            else
-            {
-                string errorMessage = null;
-                if (null != error)
-                {
-                    errorMessage = error.Message;
-                }
-                result = new BadRequestObjectResult(errorMessage + ", URL passed in = " + gitHubURL);
-            }
-            return result;
 
+            if (null != errorMessage)
+                result = new BadRequestObjectResult(errorMessage + ", URL passed in = " + gitHubURL);
+            else
+                result = new OkObjectResult(code);
+            
+            log.LogInformation("C# HTTP trigger finished");
+            return result;
         }
     }
 }
